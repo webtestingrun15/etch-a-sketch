b77b7488e0f6c184a2fac3f932f47678e2eced0c
Sheldon <webtestingrun15@users.noreply.github.com>
2025-07-14 23:07:20 -0400

    feat: create 16x16 grid and center content
    need to move .grids size to move to js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const container = document.querySelector(".container");
-const grids = document.createElement("div");
-grids.classList.add("grids");
 
+
+function grid(l,w) {
+  console.log(`${l} x ${w}`);
+  const fullGrid = l * w;
+  for (let i = 1; i <= fullGrid; i++) {
+        const grids = document.createElement("div");
+        grids.classList.add("grids");
+        container.appendChild(grids);
+  }
+}
+container.addEventListener('load', grid(16,16))
